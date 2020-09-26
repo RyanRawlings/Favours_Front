42679642ff2043bd9fd61904a847bb6c40f4ed15
Arsedian Ivan <arsedian.ivan@gmail.com>
2020-09-26 11:55:20 +1000

    Add Material UI AppBar

--- src/views/recordFavour.js
+++ src/views/recordFavour.js
@@ -3,5 +3,6 @@ import * as testAPI from "./api/TestAPI";
 import "./App.css";
 import  ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
-import { DataGrid } from '@material-ui/core/data-grid'
+import DataGrid  from '@material-ui/core/data-grid';
+import AppBar from '@material-ui/core/AppBar';
 
