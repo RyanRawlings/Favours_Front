12eb7193aaa5eec88a5641255b5c42248b1d75e5
Arsedian Ivan <arsedian.ivan@gmail.com>
2020-09-26 12:03:35 +1000

    Add React Material-UI Drawer

--- src/views/recordFavour.js
+++ src/views/recordFavour.js
@@ -5,4 +5,4 @@ import  ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import DataGrid  from '@material-ui/core/data-grid';
 import AppBar from '@material-ui/core/AppBar';
-
+import Drawer from '@material-ui/core/Drawer';
