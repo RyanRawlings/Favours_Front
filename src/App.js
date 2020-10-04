37558a1aa61f78faadf11e9475cd0549768f5492
Arsedian Ivan <arsedian.ivan@gmail.com>
2020-10-04 12:22:59 +1100

    Setup React Routers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import * as testAPI from "./api/TestAPI";
 import Routes from "./Routes";
 import { createBrowserHistory } from "history";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 const App = () => {
   
