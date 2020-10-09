f0b353edad8a3c9fe87fd03b26a6b21c08f15b2f
Arsedian Ivan <arsedian.ivan@gmail.com>
2020-10-10 08:06:06 +1100

    Create Favours List

---
+++ src/components/favours/FavoursList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import FavoursListItem from './FavoursListItem';
+
+const FavoursList = ({ favours }) => (
+    <div className = "list-wrapper">
+        {favours.map(favour => <FavoursListItem favour={favour} />)}
+
+    </div>
+);
