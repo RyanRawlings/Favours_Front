2f69b10abb1194b4adfc2828a6b8a517ad1fb3ca
Arsedian Ivanurrahman <arsedianivanurrahman@192-168-1-104.tpgi.com.au>
2020-10-18 09:43:24 +1100

    Update Record Favour's View

--- src/views/recordFavour/recordFavour.js
+++ src/views/recordFavour/recordFavour.js
@@ -21,44 +21,93 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Container from "@material-ui/core/Container";
 import UserContext from "../../context/UserContext";
 import './RecordFavour.css';
+import SaveIcon from '@material-ui/icons/Save';
 
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+      },
+    paper: {
+        padding: theme.spacing(2),
+        margin: 'auto',
+        maxWidth: 500,
+    },  
+    form: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+    },   
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 200,
+    },
+    submit: {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
 
 const RecordFavourForm = () => {
+    const classes = useStyles();
     const [favourtype, setFavourType] = useState('');
     const [debtor, setDebtor] = useState('');
     const [creditor, setCreditor] = useState('');
     const [favourstatus, setFavourStatus] = useState('');
 
     return (
-        <form>
-        <div className="record-favour-form">
-            <input className="record-favour-type" 
-                type = "text" 
-                placeholder="Select your favour type here" 
-                value = {favourtype}
-                onChange={e => setFavourType(e.target.value)} 
-            />
-            <input className="record-debtor-firstname" 
-                type = "text" 
-                placeholder="Type in your debtor name here" 
-                value = {debtor}
-                onChange={e => setDebtor(e.target.value)} 
-            />
-            <input className="record-creditor-name" 
-                type = "text" 
-                placeholder="Type in your creditor name here" 
-                value = {creditor}
-                onChange={e => setCreditor(e.target.value)} 
-            />
-            <input className="record-favour-status" 
-                type = "text" 
-                placeholder="Select your record status" 
-                value = {favourstatus}
-                onChange={e => setFavourStatus(e.target.value)} 
-            />
-            <button className="new-favour-button">Record New Favour</button>
+        <div className={classes.root}>
+            <Paper className={classes.paper}>
+                <Grid container spacing={3}>
+                    <form className={classes.form} >
+                            <TextField id='favour-type' 
+                                        label="Favour Type" 
+                                        name="FavourType" 
+                                        value={favourtype} 
+                                        placeholder="Select your Favour Type"
+                                        onChange={e => setFavourType(e.target.value)}
+                            />
+                            <TextField id='creditor-name' 
+                                        label="Paid By" 
+                                        name="Creditor" value={creditor} 
+                                        placeholder="Who paid the favour"
+                                        onChange={e => setCreditor(e.target.value)}
+                            />
+                            <TextField id='debtor-name' 
+                                        label="Owed By" 
+                                        name="Debtor" 
+                                        value={debtor} 
+                                        placeholder="Who owes the favour"
+                                        onChange={e => setDebtor(e.target.value)}
+                            />
+                            <TextField id='date' 
+                                        type="date" 
+                                        label="Paid On" 
+                                        name="PaidDate" 
+                                        className={classes,TextField} 
+                                        InputLabelProps={{shrink: true,}}
+                            />
+                            <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    size="large"
+                                    className={classes.submit}
+                                    startIcon={<SaveIcon />}
+                            >
+                                Record
+                            </Button>
+                    </form>
+                </Grid>
+            </Paper>
         </div>
-        </form>
     );
 };
 
